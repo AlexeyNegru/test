9cdd208a2a66314edf4c4a7b96a4fe6c461749b5
Alex n <Shakal240@gmail.com>
2023-07-12 21:14:27 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import userLife from './task1';
+import matchers from './task2';
+
+userLife(90);
+
+const user1 = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+matchers(user1);
---
+++ src/js/task1.js
@@ -0,0 +1,17 @@
+
+export default function userLife(user) {
+  let userHealth;
+  if (user.health >= 50) {
+    userHealth = 'healthy';
+  }
+
+  if (user.health < 50 && user.health >= 15) {
+    userHealth = 'wounded';
+  }
+
+  if (user.health < 15) {
+    userHealth = 'critical';
+  }
+
+  return userHealth;
+}
